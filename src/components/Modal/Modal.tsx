0a4ac00c6cd74204dc9f0cf4f0c616781b81de33
Fabien Stervinou <stervinoufabien@gmail.com>
2022-11-08 15:50:54 +0100

    Create useModal export

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./Modal.scss";
 
 export interface ModalProps {
   title: string;
   text: string;
   isVisible: Boolean;
+  hide: () => void;
 }
 
-const closeModal = (e: React.MouseEvent<HTMLButtonElement>) => {
+const hide = (e: React.MouseEvent<HTMLButtonElement>) => {
   e.preventDefault()
   const target = e.target as Element
   const modalTarget = target.closest('.modal') as HTMLElement | null;
@@ -17,23 +19,28 @@ const closeModal = (e: React.MouseEvent<HTMLButtonElement>) => {
   }
 };
 
-export const Modal = (props: ModalProps) => {
+const Modal = (props: ModalProps) => {
   return (
-    <div className={`modal ${props.isVisible ? " isVisible" : ""}`} aria-modal="true" role="dialog" aria-label="Interactive modal" tabIndex={-1}>
-      <div className="modalContainer">
-        <div className="modalContent">
-          <button className="modalContent-close" onClick={closeModal} tabIndex={1}>
-            &#9587;
-          </button>
-          <div className="modalContent-title">
-            {props.title}
-          </div>
-          <div className="modalContent-text">
-            {props.text}
+    props.isVisible === true 
+    ? ReactDOM.createPortal(
+      <div className={`modal ${props.isVisible === true ? " isVisible" : ""}`} aria-modal="true" role="dialog" aria-label="Interactive modal" tabIndex={-1}>
+        <div className="modalContainer">
+          <div className="modalContent">
+            <button className="modalContent-close" onClick={hide} tabIndex={1}>
+              &#9587;
+            </button>
+            <div className="modalContent-title">
+              {props.title}
+            </div>
+            <div className="modalContent-text">
+              {props.text}
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </div>,
+        document.body
+      )
+    : null
   )
 };
 
--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -1,1 +1,2 @@
 export { default } from "./Modal";
+export { useModal as useModal } from "./useModal";
