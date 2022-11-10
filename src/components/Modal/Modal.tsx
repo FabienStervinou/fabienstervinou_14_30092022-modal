525c98a704a73aef377de563676d144163be6611
Fabien Stervinou <stervinoufabien@gmail.com>
2022-11-10 08:07:55 +0100

    Lint and clean type/jest packages

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -23,21 +23,21 @@ const Modal = (props: ModalProps) => {
   return (
     props.isVisible === true 
     ? ReactDOM.createPortal(
-      <div className={`modal ${props.isVisible === true ? " isVisible" : ""}`} aria-modal="true" role="dialog" aria-label="Interactive modal" tabIndex={-1}>
-        <div className="modalContainer">
-          <div className="modalContent">
-            <button className="modalContent-close" onClick={hide} tabIndex={1}>
-              &#9587;
-            </button>
-            <div className="modalContent-title">
-              {props.title}
-            </div>
-            <div className="modalContent-text">
-              {props.text}
+        <div className={`modal ${props.isVisible === true ? " isVisible" : ""}`} aria-modal="true" role="dialog" aria-label="Interactive modal" tabIndex={-1}>
+          <div className="modalContainer">
+            <div className="modalContent">
+              <button className="modalContent-close" onClick={hide} tabIndex={1}>
+                &#9587;
+              </button>
+              <div className="modalContent-title">
+                {props.title}
+              </div>
+              <div className="modalContent-text">
+                {props.text}
+              </div>
             </div>
           </div>
-        </div>
-      </div>,
+        </div>,
         document.body
       )
     : null
